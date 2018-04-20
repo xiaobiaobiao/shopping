80492063d4033581fc660c43069e303473d7fa7e
huangbiao <54545@ii.com>
2018-04-21 00:59:16 +0800

    购物平台框架搭建

---
+++ shopping-parent/shopping-dao/src/main/java/org/shopping/dao/App.java
@@ -0,0 +1,13 @@
+package org.shopping.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
